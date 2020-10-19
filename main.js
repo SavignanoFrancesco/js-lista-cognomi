de5d2897c534d6c9521db48e33fcf7f505d9c11f
Francesco <francesco.savignano96@gmail.com>
2020-10-19 15:17:34 +0200

    repo impostata

---
+++ main.js
@@ -0,0 +1,3 @@
+//Creare un array contenente dei cognomi in ordine casuale.
+//Chiedere all'utente il suo cognome e inserirlo nella lista dei cognomi.
+//Stampare la lista dei cognomi in ordine alfabetico e comunicare all'utente la posizione in cui si trova il suo cognome (dopo averlo riordinato).
