fe360decbf12b4edc057d633aaee88f67766208b
Francesco <francesco.savignano96@gmail.com>
2020-10-19 16:08:03 +0200

    aggiunta soluzione senza l'utilizzo di indexOf()

--- main.js
+++ main.js
@@ -3,26 +3,39 @@
 //Stampare la lista dei cognomi in ordine alfabetico e comunicare all'utente la posizione in cui si trova il suo cognome (dopo averlo riordinato).
 
 //array contenente una lista di cognomi casuali
-var lista_cognomi = ['rossi','ferrari','russo','bianchi','romano','gallo','costa','fontana'];
+var surname_list = ['rossi','ferrari','russo','bianchi','romano','gallo','costa','fontana'];
 
 //Richiesta all'utente di inserire il proprio cognome
-var cognome = prompt('Inserisci il tuo cognome');
+var surname = prompt('Inserisci il tuo cognome');
+
 
 //Inserimento del cognome utente nella lista e tampa della lista disordinata
-lista_cognomi.push(cognome);
+surname_list.push(surname);
 console.log('Lista disordinata:');
-for(var i = 0; i < lista_cognomi.length; i++){
-    console.log(i, lista_cognomi[i]);
+for(var i = 0; i < surname_list.length; i++){
+    console.log(i, surname_list[i]);
 }
 
 //ordinamento alfabetico
-lista_cognomi.sort();
+surname_list.sort();
 
 //stampa della lista ordinata
 console.log('Lista ordinata:');
-for(var i = 0; i < lista_cognomi.length; i++){
-    console.log(i, lista_cognomi[i]);
+for(var i = 0; i < surname_list.length; i++){
+    console.log(i, surname_list[i]);
 }
 
 //stampa la posizione del cognome dell'utente dopo aver ordinato l'array
-console.log('Posizione di', cognome, 'nell\'array: ', lista_cognomi.indexOf(cognome));
+
+//(soluzione con metodo indexOf())
+// console.log('Posizione di', cognome, 'nell\'array: ', surname_list.indexOf(cognome));
+
+//soluzione con cicolo for(trova l'ultima posizione del cognome se è gia presente un cognome uguale in lista)
+var position = 0;
+for (var i = 0; i < surname_list.length; i++) {
+    if(surname == surname_list[i]){
+        position = i;
+    }
+}
+
+console.log(surname,'si trova in posizione',position);
